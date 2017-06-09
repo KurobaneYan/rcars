7dfb18bbb12a0280adf881e43376ee9362e70ec1
Kurobane Yan <yan.kurobane@gmail.com>
2017-06-09 15:16:57 +0300

    split app into 2 components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react'
-import Car from './Car'
+import Home from './Home/Home'
 import './App.css'
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      mostPopularCars: []
-    }
-  }
-
+export default class App extends Component {
   render () {
-    const cars = this.state.mostPopularCars.map((car, i) => {
-      return <Car key={i} car={car} />
-    })
     return (
       <div className='App'>
-        <div id='CarCards'>
-          {cars}
-        </div>
+        <Home />
       </div>
     )
   }
-
-  componentDidMount () {
-    fetch('http://localhost:1337/api/cars/mostpopular/25')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          mostPopularCars: responseJson
-        })
-      })
-      .catch((error) => console.log(error))
-  }
 }
-
-export default App
---
+++ src/Home/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Car from '../Car'
+
+export default class Home extends Component {
+  constructor () {
+    super()
+    this.state = {
+      mostPopularCars: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://localhost:1337/api/cars/mostpopular/25')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          mostPopularCars: responseJson
+        })
+      })
+      .catch((error) => console.error(error))
+  }
+
+  render () {
+    const cars = this.state.mostPopularCars.map((car, i) => {
+      return <Car key={i} car={car} />
+    })
+    return (
+      <div className='Home'>
+        <div id='CarCards'>
+          {cars}
+        </div>
+      </div>
+    )
+  }
+}
